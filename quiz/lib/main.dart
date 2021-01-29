1b6096385b288a7d54bed2ceceaaecc0c2aef059
junewoo <junewoo@cavuai.com>
2021-01-29 12:46:21 +0900

    stateful widget 으로 변경 https://github.com/korca0220/Flutter_project_second/issues/1

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -4,31 +4,50 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-void answerQuestion(){
-  print('Print answer');
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
+
+class _MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['What\'s you favorite color?', 'What \s you favorite animal?'];
+    var questions = [
+      'What\'s you favorite color?',
+      'What \s you favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('test'),
         ),
-        body: Column(children: <Widget>[
-          Text('The question!'),
-          RaisedButton(
-            child: Text('Answer 1'), onPressed: answerQuestion,
-          ),
-          RaisedButton(
-            child: Text('Answer 2'), onPressed: answerQuestion,
-          ),
-          RaisedButton(
-            child: Text('Answer 3'), onPressed: answerQuestion,
-          )
-        ],),
+        body: Column(
+          children: <Widget>[
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            )
+          ],
+        ),
       ),
     );
   }
