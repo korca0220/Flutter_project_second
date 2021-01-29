9a0e5868d2596743d6fdf667849765ea678c93d1
junewoo <junewoo@cavuai.com>
2021-01-29 17:54:01 +0900

    project base setup https://github.com/korca0220/Flutter_project_second/issues/4, https://github.com/korca0220/Flutter_project_second/issues/2

---
+++ quiz_with_django/android/app/src/main/kotlin/com/example/quiz_with_django/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quiz_with_django
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ quiz_with_django/lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Quiz App'),
+          backgroundColor: Colors.deepPurple,
+          leading: Container(),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text('test'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
