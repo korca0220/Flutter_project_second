3a5e13d1224a4c712af151f4bc0e3990ba506574
junewoo <korca02220@naver.com>
2021-02-03 16:33:03 +0900

    transaction widget 목록, 생성 위젯 추가 (기존 main에서 이동) https://github.com/korca0220/Flutter_project_second/issues/8
    - transaction 생성 가능한 텍스트필드 추가 https://github.com/korca0220/Flutter_project_second/issues/7

---
+++ personal_cost/lib/widgets/transaction_list.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:personal_cost/models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatefulWidget {
+  final List<Transaction> transactions;
+  TransactionList({this.transactions});
+
+  @override
+  _TransactionListState createState() => _TransactionListState();
+}
+
+class _TransactionListState extends State<TransactionList> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: widget.transactions.map((tx) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
+                ),
+                child: Text(
+                  '\$${tx.amount}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tx.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '${DateFormat().format(tx.date)}',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
