81bc919fd74cb9859ad593f66e9541f307de5539
junewoo <korca02220@naver.com>
2021-02-10 12:31:47 +0900

    카테고리 세부 화면 추가 https://github.com/korca0220/Flutter_project_second/issues/10
    - routes 추가
    - 화면 전환시 arguments 받는 방식 변경

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/category_meals_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,32 +32,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Navigation time',
-            ),
-          ],
-        ),
-      ),
+      routes: {
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
