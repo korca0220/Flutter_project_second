6e5a30223dd0e9cbd551ad843174d7ced27a9a71
Junewoo <korca02220@naver.com>
2021-02-22 23:51:22 +0900

    project base struct setup https://github.com/korca0220/Flutter_project_second/issues/12

---
+++ shop_app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => MyHomePage(),
+        ProdulctsOverviewScreen.pageId: (ctx) => ProdulctsOverviewScreen(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
---
+++ shop_app/lib/screens/products_overview_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProdulctsOverviewScreen extends StatelessWidget {
+  static const String pageId = '/product_overview';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
