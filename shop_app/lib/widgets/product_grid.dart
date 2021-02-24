6ceae131871c9774bc3b748cb42028f901e47208
Junewoo <korca02220@naver.com>
2021-02-25 01:20:15 +0900

    Overview 화면에서 popup button 추가  https://github.com/korca0220/Flutter_project_second/issues/13, https://github.com/korca0220/Flutter_project_second/issues/12
    - favorite 여부에 대해서 필터링 항목읠 출력하도록 기능 추가

--- shop_app/lib/widgets/product_grid.dart
+++ shop_app/lib/widgets/product_grid.dart
@@ -4,16 +4,21 @@ import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavorites;
+  ProductsGrid(this.showFavorites);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products =
+        showFavorites ? productsData.favoriteItems : productsData.items;
+    ;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
       itemBuilder: (context, index) {
-        return ChangeNotifierProvider(
-          create: (context) => products[index],
+        return ChangeNotifierProvider.value(
+          value: products[index],
           child: ProductItem(),
         );
       },
