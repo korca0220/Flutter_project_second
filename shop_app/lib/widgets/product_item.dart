03d060dd0def54fa15313f9d12004e7a9fedfbd0
Junewoo <korca02220@naver.com>
2021-02-26 00:21:49 +0900

    product 모듈 models -> provider 로 이동, 장바구니 관리할 provider 클래스 추가  https://github.com/korca0220/Flutter_project_second/issues/13

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/models/product.dart';
+import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
