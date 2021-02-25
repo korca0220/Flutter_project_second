3bd4a2c2875f2e603e802bc8ecc0190b8371ba00
Junewoo <korca02220@naver.com>
2021-02-26 01:08:40 +0900

    장바구니 화면 추가(cart screen) https://github.com/korca0220/Flutter_project_second/issues/12

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/screens/cart_screen.dart';
 
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
         routes: {
           '/': (ctx) => ProdulctsOverviewScreen(),
           ProductDetailScreen.routeId: (ctx) => ProductDetailScreen(),
+          CartScreen.routeId: (ctx) => CartScreen(),
         },
       ),
     );
--- shop_app/lib/screens/products_overview_screen.dart
+++ shop_app/lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/widgets/badge.dart';
 
 import 'package:shop_app/widgets/product_grid.dart';
@@ -54,7 +55,9 @@ class _ProdulctsOverviewScreenState extends State<ProdulctsOverviewScreen> {
             ),
             child: IconButton(
               icon: Icon(Icons.shopping_cart),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeId);
+              },
             ),
           )
         ],
