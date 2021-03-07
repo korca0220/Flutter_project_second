9062e6b0c9fbaeb626fdf5f61fbd3d9b08e7d4e6
Junewoo <korca02220@naver.com>
2021-03-07 23:45:44 +0900

    장바구니 아이템 추가시 snackbar 추가 ( 실행 취소 기능 같이 추가 )  https://github.com/korca0220/Flutter_project_second/issues/14

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -9,8 +9,7 @@ class Products with ChangeNotifier {
       title: 'Red Shirt',
       description: 'A red shirt - it is pretty red!',
       price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+      imageUrl: 'https://img.wizwid.com/PImg/706473/bsc/706473122.jpg',
     ),
     Product(
       id: 'p2',
