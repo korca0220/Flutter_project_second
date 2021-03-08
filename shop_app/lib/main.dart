68974c4ae31e6cadc32f87425135d413b662d6b7
Junewoo <korca02220@naver.com>
2021-03-09 00:26:55 +0900

    product 수정 가능한 페이지 추가(user_product_screen), drawer에 추가 https://github.com/korca0220/Flutter_project_second/issues/14

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/user_product_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,6 +43,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeId: (ctx) => ProductDetailScreen(),
           CartScreen.routeId: (ctx) => CartScreen(),
           OrdersScreen.routeId: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeId: (ctx) => UserProductsScreen(),
         },
       ),
     );
--- shop_app/lib/widgets/app_drawer.dart
+++ shop_app/lib/widgets/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/screens/orders_screen.dart';
+import 'package:shop_app/screens/user_product_screen.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -19,11 +20,21 @@ class AppDrawer extends StatelessWidget {
               Navigator.of(context).pushReplacementNamed('/');
             },
           ),
+          Divider(),
           ListTile(
             leading: Icon(Icons.credit_card),
             title: Text('Orders'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(OrdersScreen.routeId);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(UserProductsScreen.routeId);
             },
           )
         ],
