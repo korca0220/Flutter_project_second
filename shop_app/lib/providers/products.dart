30299c5d5857af9c90bf239bd6ad86d649e31ba8
Junewoo <korca02220@naver.com>
2021-03-15 00:03:59 +0900

    db에 product 데이터 추가 https://github.com/korca0220/Flutter_project_second/issues/15
    - 추후 리팩토링 예정

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:shop_app/providers/product.dart';
 
 class Products with ChangeNotifier {
@@ -53,15 +54,27 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-      id: DateTime.now().toString(),
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-    );
-    _items.add(newProduct);
-    notifyListeners();
+    const url = '';
+    http
+        .post(Uri.parse(url),
+            body: json.encode({
+              'title': product.title,
+              'description': product.description,
+              'imageUrl': product.imageUrl,
+              'price': product.price,
+              'isFavorite': product.isFavorite,
+            }))
+        .then((response) {
+      final newProduct = Product(
+        id: json.decode(response.body)['name'],
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
